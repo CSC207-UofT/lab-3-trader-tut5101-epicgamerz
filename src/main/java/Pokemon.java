21e0f53bdf0af5ee61527e8df3aae97788a191a5
Thomas <tomtom.liu@mail.utoronto.ca>
2021-09-27 19:30:24 -0400

    Gotta catch em all but with tests

--- src/main/java/Pokemon.java
+++ src/main/java/Pokemon.java
@@ -1,18 +1,39 @@
-public class Pokemon implements Tradable, Domesticatable{
+public class Pokemon implements Tradable, Domesticatable, Drivable{
 
     private String name;
+    private int maxspeed;
+
+    public Pokemon() {
+        this.name = "Pikachu";
+        this.maxspeed = 5;
+    }
 
     public Pokemon(String name) {
         this.name = name;
+        this.maxspeed = 5;
     }
 
-    @Override
     public String sound() {
-        return this.name;
+        return this.name + "!";
     }
 
     @Override
     public int getPrice() {
-        return 0;
+        return this.name.length() * 5;
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        this.maxspeed += 2;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.maxspeed -= 3;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.maxspeed;
     }
 }
